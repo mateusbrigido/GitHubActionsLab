2acb72f93e3a317fa40cf93a755fceaebac45004
Mateus Brígido <mateusbrigido@live.com>
2024-09-01 18:54:38 -0300

    Initial Commit

---
+++ GitHubActionsLab/GitHubActionsLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsLabApp.swift
+//  GitHubActionsLab
+//
+//  Created by Mateus Brigido on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
